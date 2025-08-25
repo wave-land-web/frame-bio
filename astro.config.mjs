069b9a7cadc6bdf1ba0ec666be676acbeaeae8d6
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-08-25 12:23:42 -0500

    fix(app): remove envs for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,25 +9,25 @@ import { SITE_URL } from './src/consts'
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  env: {
-    schema: {
-      PUBLIC_SANITY_STUDIO_PROJECT_ID: envField.string({
-        context: 'client',
-        access: 'public',
-        default: 'vs47sslu',
-      }),
-      PUBLIC_SANITY_STUDIO_DATASET: envField.string({
-        context: 'client',
-        access: 'public',
-        default: 'production',
-      }),
-      SANITY_STUDIO_SECRET_TOKEN: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-    },
-    validateSecrets: true,
-  },
+  // env: {
+  //   schema: {
+  //     PUBLIC_SANITY_STUDIO_PROJECT_ID: envField.string({
+  //       context: 'client',
+  //       access: 'public',
+  //       default: 'vs47sslu',
+  //     }),
+  //     PUBLIC_SANITY_STUDIO_DATASET: envField.string({
+  //       context: 'client',
+  //       access: 'public',
+  //       default: 'production',
+  //     }),
+  //     SANITY_STUDIO_SECRET_TOKEN: envField.string({
+  //       context: 'server',
+  //       access: 'secret',
+  //     }),
+  //   },
+  //   validateSecrets: true,
+  // },
   adapter: netlify({
     imageCDN: false,
     cacheOnDemandPages: true,
