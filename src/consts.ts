3b26e07734d926bc16a10b37aa663b403114c350
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-09-05 17:03:34 -0500

    feat(app): Initial copy

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Frame Bio'
-// TODO: Update the description + URL
-export const SITE_DESCRIPTION = 'Frame Bio Description Placeholder'
+export const SITE_TITLE =
+  'Frame Bio - Developing advanced regenerative therapies for bones, cartilage, and other connective tissues'
+export const SITE_DESCRIPTION =
+  'Developing next-gen therapies for bone and connective tissue disorders that target root causes using a natural form of intercellular drug delivery.'
+// TODO: Update this when we have a real domain.
 export const SITE_URL = 'http://localhost:4321'
